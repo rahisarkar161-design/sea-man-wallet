d65ca035b77de78938cc0139c04357f01891606d
rahisarkar161-design <rahisarkar161@gmail.com>
2026-05-10 21:41:16 +0530

    Delete app/_layout.js

--- app/-layout.js
+++
@@ -1,5 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function Layout() {
-  return <Stack screenOptions={{ headerStyle: { backgroundColor: '#0B4F6C' }, headerTintColor: '#fff' }} />;
-}
